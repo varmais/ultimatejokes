1d65dd2d1ca78b8c98eb848b854c1db6960030ac
varmais <teemu.tiilikainen@gmail.com>
2013-04-12 12:46:34 +0300

    initial commit and gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joke::Application.config.secret_token = '1fc9318fe5ab343f7b9e9eea47ece0789008b31781e43e03f592a4b0f9d591e07b33d17946fb1ef76138797155f2f3e3b32a888138656f031a2f60a349aa1957'
