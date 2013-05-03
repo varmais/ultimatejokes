7fcb0fbc65c8a435bdc8034b2d8329dae03089b1
varmais <teemu.tiilikainen@gmail.com>
2013-05-03 12:48:45 +0300

    added jokes tests which require user

--- test/functional/jokes_controller_test.rb
+++ test/functional/jokes_controller_test.rb
@@ -12,5 +12,54 @@ class JokesControllerTest < ActionController::TestCase
 		get :show, id: jokes(:one).id
 		assert_response :success
 	end
-  
+
+	def login_as(user)
+		@request.session[:user_id] = user ? user.id : nil
+	end
+
+	#these should only work when logged in
+	test "should get new joke page" do
+		login_as(users(:one))
+		get :new
+		assert_response :success
+	end
+
+	test "should create new joke" do
+		login_as(users(:one))
+		assert_difference('Jokes.count') do
+			joke :create, joke: 
+			{title: jokes(:one).title, content: jokes(:one).content}
+		end
+		assert_redirected_to jokes_path
+	end
+
+	test "should get edit joke page" do
+		login_as(users(:one))
+		get :edit, id: jokes(:one).id
+		assert_response :success
+	end
+
+	test "should update joke" do
+		login_as(users(:one))
+		put :update, id: jokes(:one), joke: 
+		{title: 'jooh jooh jooh'}
+		assert_response :redirect
+		assert_redirected_to :joke
+	end
+
+	test "should not update invalid joke" do
+		login_as(users(:one))
+		put :update, id: jokes(:one), joke: {title: '' }
+		assert_template :edit
+	end
+
+	test "should delete post" do
+		login_as(users(:one))
+		assert_difference('Jokes.count', -1) do
+			delete :destroy, id: posts(:one).id
+		end
+		assert_redirected_to jokes_path
+	end
+
+	
 end
