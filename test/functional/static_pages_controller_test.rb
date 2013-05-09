cb451e4d3668b5b4c4eac4ece9cac255e9d7d33f
VarmaIS <user@server>
2013-05-09 12:13:50 +0300

    fixed static pages tests

--- test/functional/static_pages_controller_test.rb
+++ test/functional/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home page" do
-  	get :index
+  	get :home
   	assert_response :success
   end
 end
