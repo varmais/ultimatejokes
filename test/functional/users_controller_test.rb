bfec2d83a4f77c861c651a9504dcf3867d531d07
Joonas <ahola.joon@gmail.com>
2013-05-10 13:58:40 +0300

    More tests for user-controller

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionController::TestCase
   setup do
     :activate_authlogic
     @user = users(:one)
+    @user2 = users(:two)
   end
 
   test "should get index" do
@@ -42,16 +43,37 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not get edit for another user" do
+    UserSession.create(users(:one))
+    get :edit, id: @user2
+    assert_redirected_to jokes_path
+
   test "should update user" do
     UserSession.create(users(:one))
     put :update, id: @user, user: @user.attributes
     assert_redirected_to user_path(assigns(:user))
   end
 
+  test "should not update another user" do
+    UserSession.create(users(:one))
+    put :update, id: @user2, user: @user2.attributes
+    assert_redirected_to jokes_path
+  end
+
+
   test "should destroy user" do
     UserSession.create(users(:one))
     assert_difference('User.count', -1) do
       delete :destroy, id: @user
+    end
+
+    assert_redirected_to users_path
+  end
+
+  test "should not destroy another user" do
+    UserSession.create(users(:one))
+    assert_no_difference('User.count') do
+      delete :destroy, id: @user2
     end
 
     assert_redirected_to users_path
