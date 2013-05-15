604c3489a68bb8375b9a2f192b8b197c42221e37
Joonas Ahola <ahola.joon@gmail.com>
2013-05-15 23:36:24 +0300

    Added possibility to link to external images or upload your own

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,6 +1,6 @@
 class Joke < ActiveRecord::Base
 	acts_as_votable
-	attr_accessible :title, :content, :user_id
+	attr_accessible :title, :content, :user_id, :image_url
 	belongs_to :user
 	validates_presence_of :title, :content
 end
